abd657db9bce89e5766db1998123a345920c83b2
BlackPBOT <109285686+BlackPBOT@users.noreply.github.com>
2022-07-29 08:44:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-//═══════════════════════════════════════════════════════//
+o//═══════════════════════════════════════════════════════//
 //
 //                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
 //𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.owner = ['94705208758']
+global.premium = ['94705208758']
+global.ownernomer = '94705208758'
+global.ownername = '🇱🇰 Black Prince'
+global.botname = 'Black Prince Bot'
+global.footer = '©BPBot.'
+global.ig = 'https://github.com/bpbot'
+global.region = 'Sri Lanka, Southern, Tangalle'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://t.me/FIZPUBLIC'
+global.packname = '🇱🇰Black Prince Pack'
+global.author = '🇱🇰Black Prince'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🇱🇰','☘','🔥']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
